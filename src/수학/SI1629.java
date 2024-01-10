870efd2b3f618d87a9e7e4b514e972a8b4c5f650
ta-mj <kiatae0722@naver.com>
2024-01-10 15:06:31 +0900

    난이도 : Silver I 제목 : 곱셈 알고리즘 : 수학/분할 정복을 이용한 거듭제곱 Comment : int/long의 범위 판단, (a * b * c) % d = (a % d) * (b % d) * (c % d)

--- src/수학/SI1629.java
+++ src/수학/SI1629.java
@@ -9,16 +9,13 @@ public class SI1629 {
     public static long c;
     public static long mod(long a,long b){
         if(b == 1){
-            return a;
+            return a % c;
         }
         long temp = mod(a,b / 2);
         if(b % 2 == 1){
-            temp = temp * temp * a;
+            return (temp * temp % c) * a % c;
         }
-        else{
-            temp = temp * temp;
-        }
-        return temp;
+        return temp * temp % c;
     }
     public static void main(String args[]) throws IOException{
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -26,6 +23,6 @@ public class SI1629 {
         long a = Integer.parseInt(st.nextToken());
         long b = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-
+        System.out.println(mod(a,b));
     }
 }
